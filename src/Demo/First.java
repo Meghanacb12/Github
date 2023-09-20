3e656764cac85278f2e42b4690b40a6913c5547e
Meghana <meghanacb22@gmail.com>
2023-09-20 10:28:59 +0530

    second

--- src/Demo/First.java
+++ src/Demo/First.java
@@ -1,5 +1,16 @@
 package Demo;
 
-public class First {
 
+public class First {
+public static void main(String[] args) {
+		
+		First c=new First();
+			}
+	
+}
+
+class Student {
+	public void study() {
+		System.out.println("students study");
+	}
 }
